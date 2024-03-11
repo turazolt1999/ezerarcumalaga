71105c7cc03a6c7954c85cf73be74ec4f7d5fe2d
TURANSKY\turan <turan.zoltan0614@gmail.com>
2024-03-11 15:20:17 +0100

    Feature: Creating 3 new page components and defining the Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    { path: 'blog', component: BlogComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
