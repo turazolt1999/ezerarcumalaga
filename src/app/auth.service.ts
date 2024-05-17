2b5e4a6bfad30e76bab5c2bb715a7ac5eaca06da
TURANSKY\turan <turan.zoltan0614@gmail.com>
2024-05-17 03:01:48 +0200

    Feature: Login completed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, inject } from "@angular/core";
-import { Auth, createUserWithEmailAndPassword, updateProfile } from "@angular/fire/auth";
+import { Injectable, inject, signal } from "@angular/core";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, user } from "@angular/fire/auth";
 import { Observable, from } from "rxjs";
+import { UserInterface } from "./user.interface";
 
 @Injectable({
     providedIn: 'root'
@@ -8,6 +9,8 @@ import { Observable, from } from "rxjs";
 
 export class AuthService {
     firebaseAuth = inject(Auth)
+    user$ = user(this.firebaseAuth)
+    currentUsersig = signal<UserInterface | null | undefined>(undefined)
 
     register(
         email: string, username: string, password: string
@@ -17,6 +20,17 @@ export class AuthService {
         ).then((res) => {
             updateProfile(res.user, { displayName: username })
         });
+        return from(promise);
+    }
+
+    login(
+        email: string, password: string
+    ): Observable <void> {
+        const promise = signInWithEmailAndPassword(
+            this.firebaseAuth,
+            email,
+            password
+        ).then(() => {});
         return from(promise);
     }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,6 +14,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   fb = inject(FormBuilder);
   http = inject(HttpClient);
+  authService = inject(AuthService);
   router = inject(Router);
 
   form = this.fb.nonNullable.group({
@@ -20,8 +22,20 @@ export class LoginComponent {
     password: ['', Validators.required],
   })
 
+  errorMessage: string | null = null;
+
   onSubmit(): void {
-    console.log("login");
+    const rawForm = this.form.getRawValue();
+    this.authService
+      .login(rawForm.email, rawForm.password)
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl('/');
+        },
+        error: (err) => {
+          this.errorMessage = err.code;
+        }
+      })
   }
 
 }
