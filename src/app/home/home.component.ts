edadd8c2b2a9c6e384f9c8038a989ec2ac4315b4
TURANSKY\turan <turan.zoltan0614@gmail.com>
2024-05-17 07:19:51 +0200

    Feature: Home komponens létrehozva

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
